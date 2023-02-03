304d4b3d52f539a933d1064d1a4ffd41b987e854
milad <mohammadreza.khorrami21@gmail.com>
2023-02-03 18:23:26 +0330

    fixed reviews bug

--- src/Routes/Reviews.js
+++ src/Routes/Reviews.js
@@ -32,124 +32,124 @@ export default function Reviews(props) {
               {ep.length > persian.length ?
                 <> 
                 {persian.slice(0,9).map((episode) => ( 
-                  <div key={episode.id} className="group relative">
-                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-zinc-900 lg:aspect-none lg:h-90">
-                    <span className="ml-4 font-medium text-sm text-gray-400">Apple Podcasts</span>
-                    <span
-                     className="ml-4 text-yellow-500 mt-4 flex fill-current"
-                    >
-                  <svg
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="2"
-                    width="36"
-                    height="36"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
+                  <div key={episode.id} className="group relative rounded-md bg-zinc-900 border-8 border-zinc-900">
+                  <div className="h-20 aspect-w-1 w-full overflow-hidden rounded-md bg-zinc-900 lg:aspect-none lg:h-90">
+                  <span className="absolute flex w-full justify-center font-medium text-sm text-gray-400">Apple Podcasts</span>
+                  <span
+                   className="ml-4 text-yellow-500 mt-8 lg:mt-4 flex fill-current"
                   >
-                    <path
-                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
-                      fill-rule="nonzero"
-                    />
-                  </svg>
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
+                    fill-rule="nonzero"
+                  />
+                </svg>
 
-                  <svg
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="2"
-                    width="36"
-                    height="36"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
-                      fill-rule="nonzero"
-                    />
-                  </svg>
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
+                    fill-rule="nonzero"
+                  />
+                </svg>
 
-                  <svg
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="2"
-                    width="36"
-                    height="36"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
-                      fill-rule="nonzero"
-                    />
-                  </svg>
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
+                    fill-rule="nonzero"
+                  />
+                </svg>
 
-                  <svg
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="2"
-                    width="36"
-                    height="36"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
-                      fill-rule="nonzero"
-                    />
-                  </svg>
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
+                    fill-rule="nonzero"
+                  />
+                </svg>
 
-                  <svg
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="2"
-                    width="36"
-                    height="36"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
-                      fill-rule="nonzero"
-                    />
-                  </svg>
-                    </span>
-                      <div className="mt-4 mb-8">
-                        <div>
-                          <p className="mt-6 ml-4 mr-1 font-bold text-base text-white">
-                            {episode.name}
-                          </p>
-                        </div>
-                        <p className="text-2xl mt-4 ml-4 mr-1 font-medium text-gray-200">
-                          {episode.describtion}
+                <svg
+                  clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  stroke-linejoin="round"
+                  stroke-miterlimit="2"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44z"
+                    fill-rule="nonzero"
+                  />
+                </svg>
+                  </span>
+                  </div>
+                    <div className="mt-4">
+                      <div>
+                        <p className=" lg:mt-6 ml-4 mr-1 font-bold text-base text-white">
+                          {episode.name}
                         </p>
                       </div>
-                      <div className="flex place-content-between">
-                      <span className="flex ml-4 font-semibold text-base">
-                        <img className="text-white" src={person} />
-                        <span style={{color: "#5b5454"}}>name</span>
-                      </span>
-                      <span className="flex mr-4 font-semibold text-base">
-                        <img className="text-white mr-1" src={calendar} />
-                        <span style={{color: "#5b5454"}}>{episode.date}</span>
-                      </span>
-                      </div>
-                      <div className="mt-8 mb-4 flex place-content-between">
-                      <div className="flex ml-4 font-semibold text-base">
-                <TwitterShareButton url={"/"}>
-                  <TwitterIcon size={40} round={true} />
-                </TwitterShareButton>
-                      </div>
-                      <div className="flex mr-4 font-semibold text-base">
-                      <FacebookShareButton url={"/"}>
-                        <FacebookIcon size={40} round={true} />
-                      </FacebookShareButton>
-                      </div>
-                      </div>
+                      <p className="text-lg lg:text-2xl mt-4 ml-4 mr-1 font-medium text-gray-200">
+                        {episode.describtion}
+                      </p>
+                    </div>
+                    <div className="flex place-content-between items-end">
+                    <span className="flex ml-4 font-semibold text-xs lg:text-base">
+                      <img className="text-white" src={person} />
+                      <span style={{color: "#5b5454"}}>name</span>
+                    </span>
+                    <span className="flex mr-4 font-semibold text-xs lg:text-base">
+                      <img className="text-white mr-1" src={calendar} />
+                      <span style={{color: "#5b5454"}}>{episode.date}</span>
+                    </span>
+                    </div>
+                    <div className="flex place-content-between items-end">
+                    <div className="flex ml-4 font-semibold text-base">
+              <TwitterShareButton url={"/"}>
+                <TwitterIcon size={40} round={true} />
+              </TwitterShareButton>
+                    </div>
+                    <div className="flex mr-4 font-semibold text-base">
+              <FacebookShareButton url={"/"}>
+                <FacebookIcon size={40} round={true} />
+              </FacebookShareButton>
+                    </div>
                     </div>
                   </div>
                 ))}
@@ -157,11 +157,11 @@ export default function Reviews(props) {
                 : 
                 <>
                 {ep.map((episode) => ( 
-                  <div key={episode.id} className="group relative">
-                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-zinc-900 lg:aspect-none lg:h-90">
-                    <span className="ml-4 font-medium text-sm text-gray-400">Apple Podcasts</span>
+                  <div key={episode.id} className="group relative rounded-md bg-zinc-900 border-8 border-zinc-900">
+                    <div className="h-20 aspect-w-1 w-full overflow-hidden rounded-md bg-zinc-900 lg:aspect-none lg:h-90">
+                    <span className="absolute flex w-full justify-center font-medium text-sm text-gray-400">Apple Podcasts</span>
                     <span
-                     className="ml-4 text-yellow-500 mt-4 flex fill-current"
+                     className="ml-4 text-yellow-500 mt-8 lg:mt-4 flex fill-current"
                     >
                   <svg
                     clip-rule="evenodd"
@@ -243,27 +243,28 @@ export default function Reviews(props) {
                     />
                   </svg>
                     </span>
-                      <div className="mt-4 mb-8">
+                    </div>
+                      <div className="mt-4">
                         <div>
-                          <p className="mt-6 ml-4 mr-1 font-bold text-base text-white">
+                          <p className=" lg:mt-6 ml-4 mr-1 font-bold text-base text-white">
                             {episode.name}
                           </p>
                         </div>
-                        <p className="text-2xl mt-4 ml-4 mr-1 font-medium text-gray-200">
+                        <p className="text-lg lg:text-2xl mt-4 ml-4 mr-1 font-medium text-gray-200">
                           {episode.describtion}
                         </p>
                       </div>
-                      <div className="flex place-content-between">
-                      <span className="flex ml-4 font-semibold text-base">
+                      <div className="flex place-content-between items-end">
+                      <span className="flex ml-4 font-semibold text-xs lg:text-base">
                         <img className="text-white" src={person} />
                         <span style={{color: "#5b5454"}}>name</span>
                       </span>
-                      <span className="flex mr-4 font-semibold text-base">
+                      <span className="flex mr-4 font-semibold text-xs lg:text-base">
                         <img className="text-white mr-1" src={calendar} />
                         <span style={{color: "#5b5454"}}>{episode.date}</span>
                       </span>
                       </div>
-                      <div className="mt-8 mb-4 flex place-content-between">
+                      <div className="flex place-content-between items-end">
                       <div className="flex ml-4 font-semibold text-base">
                 <TwitterShareButton url={"/"}>
                   <TwitterIcon size={40} round={true} />
@@ -275,7 +276,6 @@ export default function Reviews(props) {
                 </FacebookShareButton>
                       </div>
                       </div>
-                    </div>
                   </div>
                 ))}
                 </>
